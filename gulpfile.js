46247fdfd1ba1a1e4a0213bb52232a54bbfa9667
sonichuang <sonichuang@tencent.com>
2016-04-13 19:55:57 +0800

    page update

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,13 @@
 /*
  * Gulp Plugins
  */
-var gulp 		  = require('gulp'),
+var gulp      = require('gulp'),
+    Promise   = require('promise'),
+    del       = require('del'),
     connect   = require('gulp-connect');
     /*
     minifyCSS = require('gulp-minify-css'),
     replace   = require('gulp-replace'),
-    del       = require('del'),
     concat    = require('gulp-concat'),
     convertEncoding = require('gulp-convert-encoding');*/
 
@@ -21,7 +22,8 @@ var gulp 		  = require('gulp'),
  * Setting Pathsstella
  */
 var paths = {
-	src	: "src/"
+	src	: "src/",
+    dist : "dist/document/html/"
 };
 
 /* ---------------------------------------------------
@@ -29,14 +31,15 @@ var paths = {
  * https://github.com/avevlad/gulp-connect
  */
 gulp.task('connect', function() {
-  connect.server({
-    // root: paths.gameName + paths.act,
-    root: paths.src,
-    livereload: true
-  });
+   return new Promise(function (resolve, reject) {
+    connect.server({
+      root: paths.src,
+      livereload: true
+    });
+ });
 });
 gulp.task('reload-html', function () {
-  gulp.src([paths.src+'**/*.htm',paths.src+'**/*.html'])
+  return gulp.src([paths.src+'**/*.htm',paths.src+'**/*.html'])
     .pipe(connect.reload());
 });
 
@@ -45,18 +48,34 @@ gulp.task('reload-html', function () {
  * https://github.com/floatdrop/gulp-watch
  */
 gulp.task('watch-file', function () {
-    gulp.watch(paths.src+'css/**/*.css', ['reload-html']);
-    gulp.watch([paths.src+'**/*.html',paths.src+'**/*.htm'], ['reload-html']);
-    // gulp.watch(paths.currentAct+'js/**/*.js', ['minify-css','reload-html']);
+    return gulp.watch([paths.src+'**/*.html',paths.src+'**/*.htm', paths.src+'css/**/*.css'], gulp.parallel('reload-html'));
 });
 
 
+/* ---------------------------------------------------
+ * Task demo Build [向/dist/document/html/ 目录输出相关文件]
+ */
+gulp.task('svn-clean', function(done) {
+   return del([paths.dist+'**/*.*'],done);
+});
 
+gulp.task('svn-copy', function(){
+  return gulp.src([paths.src+'**/*'])
+    .pipe(gulp.dest(paths.dist))
+});
 
 
 /* ---------------------------------------------------
  * 自动化组合
  */ 
-gulp.task('watch',['connect','watch-file'])                 //文件更新自动 F5
+// gulp 4.0 改写
+gulp.task('watch',gulp.parallel('connect','watch-file'));              //专题文件更新自动 F5 
+gulp.task('build-svn',gulp.series('svn-clean','svn-copy'));
+
+
+
+
+
+
 
 
